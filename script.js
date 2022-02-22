e2cbc73bd428e46d69e081755cae5051f69a5117
Alex-Rockwell <vin-ag@list.ru>
2022-02-22 13:37:49 +0400

    2

--- script.js
+++ script.js
@@ -7,76 +7,33 @@
 // eight(split(two())); 
 // должно вернуть 4
 
-let plus = (arg) => {
-  return ['plus', arg]
-}
-let minus = (arg) => {
-  return ['minus', arg]
-}
-let multiply = (arg) => {
-  return ['multiply', arg]
-}
-let split = (arg) => {
-  return ['split', arg]
-}
+let plus = (arg) => ['plus', arg]
+let minus = (arg) => ['minus', arg]
+let multiply = (arg) => ['multiply', arg]
+let split = (arg) => ['split', arg]
 
 let act = (num1, arg) => {
   let [action, num2] = [...arg]
   switch (action) {
-    case 'plus': 
-      return num1 + num2
-    case 'minus': 
-      return num1 - num2
-    case 'multiply': 
-      return num1 * num2
-    case 'split': 
-      return Math.floor(num1 / num2)
+    case 'plus': return num1 + num2
+    case 'minus': return num1 - num2
+    case 'multiply': return num1 * num2
+    case 'split': return Math.floor(num1 / num2)
   }
 }
 
-let one = (arg) => {
-  if (!arg) return Number(1) 
-  return act(1, arg)
-}
-let two = (arg) => {
-  if (!arg) return Number(2) 
-  return act(2, arg)
-}
-let three = (arg) => {
-  if (!arg) return Number(3) 
-  return act(3, arg)
-}
-let four = (arg) => {
-  if (!arg) return Number(4) 
-  return act(4, arg)
-}
-let five = (arg) => {
-  if (!arg) return Number(5) 
-  return act(5, arg)
-}
-let six = (arg) => {
-  if (!arg) return Number(6) 
-  return act(6, arg)
-}
-let seven = (arg) => {
-  if (!arg) return Number(7) 
-  return act(7, arg)
-}
-let eight = (arg) => {
-  if (!arg) return Number(8) 
-  return act(8, arg)
-}
-let nine = (arg) => {
-  if (!arg) return Number(9) 
-  return act(9, arg)
-}
+let one = (arg) => !arg ? Number(1) : act(1, arg)
+let two = (arg) => !arg ? Number(2) : act(2, arg)
+let three = (arg) => !arg ? Number(3) : act(3, arg)
+let four = (arg) => !arg ? Number(4) : act(4, arg)
+let five = (arg) => !arg ? Number(5) : act(5, arg)
+let six = (arg) => !arg ? Number(6) : act(6, arg)
+let seven = (arg) => !arg ? Number(7) : act(7, arg)
+let eight = (arg) => !arg ? Number(8) : act(8, arg)
+let nine = (arg) => !arg ? Number(9) : act(9, arg)
 
-
-
-let res = six(minus(five())); 
-let res2 = one(plus(six()))
-let res3 = six(split(two()))
-console.log(res3)
+let res = eight(split(two()))
+console.log(res)
 
 
 
